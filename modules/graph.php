cd03072b17cc6812e150451d37717263ad5de8ec
xepheris <admin@Gerrits-iMac.local>
2016-11-09 13:41:58 +0100

    new proper graph!

--- modules/graph.php
+++ modules/graph.php
@@ -1,6 +1,7 @@
 <?php
-if(isset($_GET['updategraph']) && ($_GET['updategraph'] == '1')) {
-		
+
+if(isset($_GET['updategraph']) && $_GET['updategraph'] == '1') {
+	
 	$valid_update_query = mysqli_fetch_array(mysqli_query($conn, "SELECT `users` FROM `total_graph_data` WHERE `total` = '101'"));
 	$update_diff = time('now')-$valid_update_query['users'];
 				
@@ -9,46 +10,74 @@ if(isset($_GET['updategraph']) && ($_GET['updategraph'] == '1')) {
 		// KILL OLD GRAPH TABLE
 		$kill_old_graph_table = mysqli_query($conn, "TRUNCATE TABLE `total_graph_data`");
 		
-		// SELECT HIGHEST PERCENTAGE AND ROUND UP TO NEXT QUARTER PERCENT AS CAP FOR CURRENT GRAPH
-		$select_highest_total = mysqli_fetch_array(mysqli_query($conn, "SELECT `total` FROM `data1` ORDER BY `total` DESC LIMIT 1"));
-		$highest_total = $select_highest_total['total'];
+		$rankone = mysqli_fetch_array(mysqli_query($conn, "SELECT `total`, `char`, `region`, `server` FROM `data1` ORDER BY `total` DESC LIMIT 1"));
+		$server = $rankone['server'];
+		$char = $rankone['char'];
+		$region = $rankone['region'];
+		$total = $rankone['total'];
 		
-		// BUILD NEW TABLE CONTENT
-		$factor = '0.95';
+		// ENABLE SSL
+		$arrContextOptions=array('ssl' => array('verify_peer' => false, 'verify_peer_name' => false, ),);  
+	
+		// API KEY
+		$api = '';
+	
+		// REMOVE SPACES IN SERVER NAME TO PREVENT BUGS IN URL
+		if(strpos($server, ' ') !== false) {
+			$server = str_replace(' ', '%20', $server);
+		}
+		// REMOVE SLASHES IN SERVER NAME TO ALLOW ACTUAL SEARCH AGAIN
+		$server = stripslashes($server);
+	
+		// ARMORY API LINK ACHIEVEMENTS
+		$url = 'https://' .$region. '.api.battle.net/wow/character/' .$server. '/' .$char. '?fields=achievements&locale=en_GB&apikey=' .$api. '';
+	
+		$data = @file_get_contents($url, false, stream_context_create($arrContextOptions));
+		$data = json_decode($data, true);
+		$key = array_search('10671', $data['achievements']['achievementsCompleted']);
+
+		$criterias = array();
+		array_push($criterias, $data['achievements']['achievementsCompletedTimestamp']);
+		$criterias = $criterias['0'];
+		$timestamp = $criterias[$key];	
+		
+		// FETCH LV110 TIMESTAMP FROM RANK 1
+		$timestamp = substr($timestamp, '-13', '10');
+	
+		// CALC DAILY GAIN FROM RANK 1
+		$dayspassed = (time('now')-$timestamp)/60/60/24;
+		
+		$daily = round($total/$dayspassed, 0);
 		
 		$spam_prevention = mysqli_query($conn, "INSERT INTO `total_graph_data` (`total`, `users`) VALUES ('101', '" .time('now'). "')");
 		
-		function generate_graph_data($highest_total) {
-			global $conn, $minus;
-			
-			$factor = '0.95';
-					
-			if($highest_total >= '1000') {
-				$highest_total = round($highest_total, 0);
-				$next_lower = round($highest_total*$factor, 0);
-				
-				$fetch_amount_of_users_sql = "SELECT COUNT(`id`) AS `sum_users` FROM `data1` WHERE `total` <= '" .$highest_total. "' AND `total` > '" .$next_lower. "'";
-				$fetch_amount_of_users = mysqli_fetch_array(mysqli_query($conn, $fetch_amount_of_users_sql));
-				
-				$insert_into_graph_table_sql = "INSERT INTO `total_graph_data` (`total`, `users`) VALUES ('" .$highest_total. "', '" .$fetch_amount_of_users['sum_users']. "')";
-				$insert_into_graph_table = mysqli_query($conn, $insert_into_graph_table_sql);
-				
-				$highest_total = $highest_total*$factor;
-				
-				generate_graph_data($highest_total);
-				
-			}
-		}
+		// BUILD NEW TABLE
 		
-		generate_graph_data($highest_total);
+		function generate_graph_data($total) {
+			global $conn, $daily;		
+			$next_lower = $total-$daily;
+				
+			$fetch_amount_of_users_sql = "SELECT COUNT(`id`) AS `sum_users` FROM `data1` WHERE `total` <= '" .$total. "' AND `total` > '" .$next_lower. "'";
+			$fetch_amount_of_users = mysqli_fetch_array(mysqli_query($conn, $fetch_amount_of_users_sql));
+				
+			$insert_into_graph_table_sql = "INSERT INTO `total_graph_data` (`total`, `users`) VALUES ('" .$total. "', '" .$fetch_amount_of_users['sum_users']. "')";
+			$insert_into_graph_table = mysqli_query($conn, $insert_into_graph_table_sql);
+				
+			$total = $next_lower;
+				
+			generate_graph_data($total);
+				
+		}
+
+		generate_graph_data($total);
 		
 		echo '<p style="color: green;">Thanks for updating!</p>';
+		
 	}
 	elseif($update_diff < '3600') {
 		echo '<p id="error">Sorry, updating the graph is allowed only once every one hour. Next update possible in: ' .(3600-$update_diff). ' seconds.</p>';
 	}
 }
-			
 echo '<div id="graph_users" style="width: 90%; height: 500px"></div>';
 
 $last_update = mysqli_fetch_array(mysqli_query($conn, "SELECT `users` FROM `total_graph_data` WHERE `total` = '101'"));
